5caa99519855696e4e98cbae794abd22b8312c01
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-11-02 20:03:22 +0000

    Match ADXERR_EntryErrFunc() + set correct compiler flags to decompile CRIware

--- src/cri/libadxe/adx_errs.c
+++ src/cri/libadxe/adx_errs.c
@@ -1,9 +1,21 @@
+#include <cri_xpt.h>
 
+Sint8 adxerr_msg[256] = { 0 }; 
+void (*adxerr_func)(void *obj, const Sint8 *msg);
+void *adxerr_obj;
 
 // ADXERR_ItoA
 // ADXERR_ItoA2
 // ADXERR_CallErrFunc2
 // ADXERR_CallErrFunc1
-// ADXERR_EntryErrFunc
+
+void ADXERR_EntryErrFunc(void (*func)(), void *obj)
+{
+    adxerr_func = func; 
+    adxerr_obj = obj;
+
+    SVM_SetCbErr(func, obj);
+}
+
 // ADXERR_Finish
 // ADXERR_Init
