e77e20731382b3cab3361eabec6190df23f3d6b4
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-11-02 19:53:02 +0000

    Match njFogEnable(), njFogDisable() and njSetFogTable()

--- src/ngc/veronica/prog/ngc_NaFog.c
+++ src/ngc/veronica/prog/ngc_NaFog.c
@@ -1,9 +1,34 @@
+#include "structs.h"
 
+unsigned int ulNaFogState;
+float* fpNaFogTblTop;
 
-// njFogEnable
-// njFogDisable
-// njSetFogColor
-// njSetFogTable
+float lbl_80488154[3];
+float lbl_804ED280;
+
+void	njFogEnable()
+{ 
+    ulNaFogState = 1; 
+
+    if (lbl_80488154[2] > lbl_804ED280)
+    {
+        fn_801CC6D4();
+    }
+} 
+
+void	njFogDisable()
+{ 
+    ulNaFogState = 0; 
+
+    fn_801CC67C();
+} 
+
+void	njSetFogTable(NJS_FOG_TABLE fog)
+{
+    fpNaFogTblTop = fog; 
+} 
+
 // njGenerateFogTable3
-// njCalcFogPowerEx
 // njCalcFogPower
+// njCalcFogPowerEx
+// njSetFogColor
