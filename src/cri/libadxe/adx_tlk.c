40ed1e78ccb9ae60b098da9f471aafc83d6b3844
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-11-03 13:38:22 +0000

    Match ADXT_GetErrCode(), ADXT_SetAutoRcvr(), ADXT_SetReloadSct(), ADXT_SetSvrFreq(), ADXT_GetOutPan() and ADXT_GetStat()

--- src/cri/libadxe/adx_tlk.c
+++ src/cri/libadxe/adx_tlk.c
@@ -1,12 +1,55 @@
 #include <cri_adxt.h>
 
+// ADXT_Pause
+
+Sint32 ADXT_GetErrCode(ADXT adxt)
+{
+    if (adxt == NULL) 
+    {
+        ADXERR_CallErrFunc1((const Sint8*)"placeholder");
+
+        return ADXT_ERR_SHRTBUF;
+    }
+
+    return adxt->ercode;
+}
+
+// fn_80243EC4
+
+void ADXT_SetAutoRcvr(ADXT adxt, Sint32 rmode)
+{
+    adxt->autorcvr = rmode;
+}
+
+void ADXT_SetReloadSct(ADXT adxt, Sint32 minsct)
+{
+    if (adxt == NULL) 
+    {
+        ADXERR_CallErrFunc1((const Sint8*)"placeholder");
+        return;
+    }
+
+    adxt->minsct = minsct;
+}
+
+void ADXT_SetSvrFreq(ADXT adxt, Sint32 freq)
+{
+    if (adxt == NULL) 
+    {
+        ADXERR_CallErrFunc1((const Sint8*)"placeholder");
+        return;
+    }
+    
+    adxt->svrfreq = freq;
+}
+
 Sint32 ADXT_GetOutVol(ADXT adxt) 
 {
     if (adxt == NULL) 
     {
-        ADXERR_CallErrFunc1("placeholder");
+        ADXERR_CallErrFunc1((const Sint8*)"placeholder");
 
-        return 0;
+        return ADXT_ERR_OK;
     }
 
     return adxt->outvol;
@@ -17,7 +60,7 @@ void ADXT_SetOutVol(ADXT adxt, Sint32 vol)
 {
     if (adxt == NULL) 
     {
-        ADXERR_CallErrFunc1("placeholder");
+        ADXERR_CallErrFunc1((const Sint8*)"placeholder");
         return;
     }
 
@@ -25,3 +68,39 @@ void ADXT_SetOutVol(ADXT adxt, Sint32 vol)
 
     ADXRNA_SetOutVol(adxt->rna, adxt->outvol + ADXSJD_GetDefOutVol(adxt->sjd));
 }
+
+Sint32 ADXT_GetOutPan(ADXT adxt, Sint32 ch) 
+{
+    if (adxt == NULL) 
+    {
+        ADXERR_CallErrFunc1((const Sint8*)"placeholder");
+
+        return ADXT_ERR_OK;
+    }
+
+    return adxt->outpan[ch];
+}
+
+// ADXT_SetOutPan
+// fn_80244180
+// fn_802441D8
+// fn_80244230
+// ADXT_GetTime
+
+Sint32 ADXT_GetStat(ADXT adxt)
+{
+    if (adxt == NULL) 
+    {
+        ADXERR_CallErrFunc1((const Sint8*)"placeholder");
+
+        return ADXT_ERR_SHRTBUF;
+    }
+
+    return adxt->stat;
+}
+
+// ADXT_Stop
+// fn_802448B8
+// fn_80244AA4
+// ADXT_Destroy
+// ADXT_Create
