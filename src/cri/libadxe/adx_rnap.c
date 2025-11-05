d8df6967c19f859fad04bf1a739de161144137cb
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-11-05 13:14:40 +0000

    Match ADXRNA_Create() + redefine adx_tlk boundaries + identify more CRI functions

--- src/cri/libadxe/adx_rnap.c
+++ src/cri/libadxe/adx_rnap.c
@@ -1,8 +1,10 @@
 #include <cri_adxt.h>
 
+typedef void* NGCRNA; // not sure about the handle name here
+
 void ADXRNA_SetOutVol(ADXRNA rna, Sint32 vol)
 {
-    NGCRNA_SetOutVol(rna, vol); // not sure about the function name here
+    NGCRNA_SetOutVol(rna, vol); 
 }
 
 void ADXRNA_SetPlaySw(ADXRNA rna, Sint32 sw)
@@ -21,4 +23,10 @@ void ADXRNA_Destroy(ADXRNA rna)
     NGCRNA_SetTransSw(rna, 0);
     
     NGCRNA_Destroy(rna);
+}
+
+NGCRNA NGCRNA_Create(SJ *sj, Sint32 maxnch); // TODO: remove this function declaration
+ADXRNA ADXRNA_Create(SJ *sj, Sint32 maxnch)
+{
+    return NGCRNA_Create(sj, maxnch);
 }
