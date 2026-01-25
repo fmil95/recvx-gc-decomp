d27c29b90abcbae745718cf5b55773d59b3a1e2c
Frozen Burnside <frozenburnside@hotmail.com>
2026-01-25 13:50:23 -0300

    Define ngc_event split

--- configure.py
+++ configure.py
@@ -353,6 +353,7 @@ config.libs = [
             Object(NonMatching, "ngc/veronica/prog/ngc_Ninjapad.c"),  # ready to match
             Object(NonMatching, "ngc/veronica/prog/main.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/ngc_sg_sd.c"),  # needs decompiling
+            Object(NonMatching, "ngc/veronica/prog/ngc_event.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/gdlib.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/ngc_NaDebugFontFunction.c"),  # needs decompiling
         ],
---
+++ src/ngc/veronica/prog/ngc_event.c
@@ -0,0 +1,4 @@
+
+
+// bhChangeBackColor
+// bhChangeBackColorEvt
