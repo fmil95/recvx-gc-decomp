17cfd7ba96efbb3de6a4a141cf6c07e32478092c
Frozen Burnside <frozenburnside@hotmail.com>
2026-02-24 13:14:46 -0300

    Define subpl split

---
+++ src/ngc/veronica/prog/subpl.c
@@ -0,0 +1,21 @@
+
+
+// bhSubpl 
+// init_subpl 
+// em60_init 
+// move_subpl 
+// em_sce 
+// bhEne_Event 
+// bhEne_Event2 
+// mv00_subpl0 
+// mv00_subpl0Ex
+// mv00_subpl1 
+// mv00_subpl2 
+// mv00_subpl2_0 
+// mv00_subpl3 
+// mv00_subpl3_0 
+// mv00_subpl5 
+// mv00_subpl6 
+// mv00_subpl0_1 
+// bhSub_DirTarget
+// mv00_subpl10 
