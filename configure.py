f526f27adb8bd00d6a0c9e4d35d65332e65e3fb0
Frozen Burnside <frozenburnside@hotmail.com>
2026-02-25 15:24:00 -0300

    Define zonzon split

--- configure.py
+++ configure.py
@@ -355,6 +355,7 @@ config.libs = [
             Object(NonMatching, "ngc/veronica/prog/system.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/vibman.c"),  # ready to match
             Object(NonMatching, "ngc/veronica/prog/weapon.c"),  # ready to match
+            Object(NonMatching, "ngc/veronica/prog/zonzon.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/zonzon1.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/ngc_NaFog.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/ngc_Ninjapad.c"),  # ready to match
