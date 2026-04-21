ce36be049f01024d8beeb2d5a4d99d503f6df618
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-21 09:35:42 -0300

    Define fileview split

---
+++ src/ngc/veronica/prog/fileview.c
@@ -0,0 +1,24 @@
+
+
+// ControlFileView
+// FileSelect
+// FileFlagInit01
+// FileFlagInit
+// GetFile
+// FileViewInit
+// FileViewMain
+// FileViewExit
+// FileExit00
+// FileExit01
+// SearchTag
+// SelectFile
+// SelectTag
+// FileScreenCancel
+// FileScrollSet
+// FileGetWait
+// PageScroll
+// FileEtcDisplay
+// StatusToFileView
+// ReadFstx
+// FileNumberSwitch
+// PlayPageCheck
