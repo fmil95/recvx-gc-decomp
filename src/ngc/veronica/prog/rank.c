d3935807c9585f66c21a92aa2ff9cf541d3d9f24
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-24 11:40:16 -0300

    Define rank split

---
+++ src/ngc/veronica/prog/rank.c
@@ -0,0 +1,26 @@
+
+
+// ControlRanking 
+// RankingInit 
+// RankingVmsWait 
+// RankingSave 
+// RankingErrorMessage
+// RankingMain 
+// RankingExit 
+// RankingTextureInit 
+// WallPaperDisp 
+// DispRank 
+// DispRankingData00 
+// DispRankingData01 
+// DispNumber 
+// DispTime 
+// AllRanking 
+// GameClearScore 
+// RodorigoEventScore 
+// SteveEventScore 
+// MapScore 
+// HealItemUseScore  
+// SaveCountScore 
+// RetryCountScore 
+// GetMessage 
+// RankingBgmSet 
