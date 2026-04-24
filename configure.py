de038574749a8812e434c4d91a869bb9d732ba36
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-24 11:41:44 -0300

    Fix split name

--- configure.py
+++ configure.py
@@ -354,7 +354,7 @@ config.libs = [
             Object(NonMatching, "ngc/veronica/prog/pad.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/padman.c"),  # functions matched, but there is extra bss
             Object(NonMatching, "ngc/veronica/prog/player.c"),  # needs decompiling
-            Object(NonMatching, "ngc/veronica/prog/rank.c"),  # needs decompiling
+            Object(NonMatching, "ngc/veronica/prog/ranking.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/room.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/rutchk.c"),  # needs decompiling
             Object(NonMatching, "ngc/veronica/prog/sbinit.c"),  # functions matched, but sbss is undefined
