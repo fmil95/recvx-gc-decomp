a60588d38e5fc8155d4f80b1b273303c4bc37047
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-24 12:41:18 -0300

    Define itemview split

---
+++ src/ngc/veronica/prog/itemview.c
@@ -0,0 +1,24 @@
+
+
+// DrawSubItem 
+// Model_Read_Start 
+// Model_Read_Set 
+// CameraInit 
+// CameraSet 
+// ItemView 
+// ItemModelChangeZoomIn 
+// ItemModelCheck 
+// ItemModelActionSet 
+// ItemModelChangeZoomOut
+// ItemModelMessageWait 
+// MdlDirChk 
+// MdlAction00 
+// MdlAction01 
+// MdlAction02 
+// MdlEvalflagsSet 
+// MdlHideCheck 
+// ModelScaleSet 
+// FlagErase 
+// LighterOpen 
+// FileSyu 
+// MakeTag
