73a690d3e24663879775f4acf4f1cc2a2b8d0342
Frozen Burnside <frozenburnside@hotmail.com>
2026-05-17 12:13:04 -0300

    Define hitchkl split

---
+++ src/ngc/veronica/prog/hitchkl.c
@@ -0,0 +1,13 @@
+
+
+// bhCollisionCheckLine 
+// bhCollisionCheckLine2 
+// bhCollisionCheckLine3 
+// bhCollisionCheckLineMain
+// bhCollisionCheckL2PL 
+// bhInOutCheck 
+// bhCollisionCheckL2MDL 
+// bhCollisionCheckL2XZPL 
+// bhCollisionCheckL2XYPL 
+// bhCollisionCheckL2YZPL 
+// bhGetHitCollisionNormal 
