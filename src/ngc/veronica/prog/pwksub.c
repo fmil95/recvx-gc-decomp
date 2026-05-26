fbed0ed3c83c0dfb3312b97ddfe850ae5051489a
Frozen Burnside <frozenburnside@hotmail.com>
2026-05-26 18:50:05 -0300

    Define pwksub split

---
+++ src/ngc/veronica/prog/pwksub.c
@@ -0,0 +1,28 @@
+
+
+// bhGetFreeMemory  
+// bhCalcLinkModel  
+// bhSetFloorNum 
+// bhCheckFloorNum  
+// bhAddSpeed 
+// bhGetFrameNum 
+// bhCalcLockEneYR  
+// bhSearchNearEnemy 
+// bhSearchNearEnemyB
+// bhSearchNearEnemy2
+// bhSearchNextEnemy 
+// bhSearchPlayer 
+// bhCheckL2Wall 
+// bhCheckC2Wall 
+// bhCheckC2WallN 
+// bhCheckClipModel 
+// bhCheckClipPoint 
+// bhSetWaterSplash 
+// bhSetWaterSplash2 
+// bhSetWaterSplash3 
+// bhSetWaterSplash4 
+// bhSetGunFire 
+// bhSetYakkyou 
+// bhSetMagazine 
+// bhSetLighterFire 
+// ControlRanking 
