f689ecc7ed0174a28597377c2224b93588e77dd7
Frozen Burnside <frozenburnside@hotmail.com>
2026-06-21 19:35:20 -0300

    Define sp_evt split

---
+++ src/ngc/veronica/prog/sp_evt.c
@@ -0,0 +1,22 @@
+
+
+// bhControlSpEvtComputer 
+// bhKeepSpEvtComputer 
+// bhInitSpEvtComputer 
+// bhMainSpEvtComputer 
+// bhExitSpEvtComputer 
+// bhEntrySpEvtComputer 
+// bhDrawSpEvtComputer 
+// bhDrawSpEvtComTime
+// bhDrawSpEvtComVal 
+// bhDrawSpEvtComBar 
+// bhDrawSpEvtComCharacter 
+// bhSetSpEvtComFade 
+// bhCalcSpEvtComFade 
+// bhDrawSpEvtComFade 
+// bhEntrySpEvtBoxLine 
+// bhDrawSpEvtBoxLine 
+// bhClearComEvtText 
+// bhInitComEvtScript 
+// bhControlComEvtScript 
+// bhControlComEvtKeyboard 
